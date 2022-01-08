34e35fa5884a83a49089925f826b6154f6cc9858
Puskás Gellért <devgellert@gmail.com>
2022-01-08 13:08:06 +0100

    menu, api service

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -4,7 +4,7 @@ import {HttpClient} from "@angular/common/http";
 @Injectable({
   providedIn: 'root'
 })
-export class ApiServiceService {
+export class ApiService {
   constructor(private http: HttpClient) { }
 
   public getPizzas() {
