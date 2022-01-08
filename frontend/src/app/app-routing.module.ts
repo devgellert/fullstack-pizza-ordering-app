3cdd2122ca2e42f791d882fe99fc95a7f6c0761e
Puskás Gellért <devgellert@gmail.com>
2022-01-08 12:15:28 +0100

    admin and client components; routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AdminComponent} from "./admin/admin.component";
+import {ClientComponent} from "./client/client.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: AdminComponent,
+    path: 'admin',
+  },
+  // {
+  //   component: ClientComponent,
+  //   path: ''
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: ""
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { AdminComponent } from './admin/admin.component';
+import { ClientComponent } from './client/client.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
@@ -18,6 +22,7 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
