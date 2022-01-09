2c12b05bb949d8d80ccf8a810198f2160fef0640
Puskás Gellért <devgellert@gmail.com>
2022-01-09 09:38:21 +0100

    dehydrate

--- frontend/src/app/services/pizza-view.service.ts
+++ frontend/src/app/services/pizza-view.service.ts
@@ -1,23 +1,24 @@
 import { Injectable } from '@angular/core';
-import {ApiService} from "./api.service";
-import {ApiPizza} from "../types/api";
+import { ApiService } from './api.service';
+import { ApiPizza } from '../types/api';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PizzaViewService {
-
   private pizza: ApiPizza | null = null;
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {}
 
-  hydratePizza (pizzaId: number) {
-    this.apiService.getPizza(pizzaId).then(pizza => {
-      this.pizza = pizza;
-    });
+  async hydratePizza(pizzaId: number) {
+    this.pizza = await this.apiService.getPizza(pizzaId);
   }
 
   public getPizza() {
     return this.pizza;
+  }
+
+  dehydratePizza() {
+    this.pizza = null;
   }
 }
