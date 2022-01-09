fbda1ec1bd7836239b7af9306af2f71de9e5c4dd
Puskás Gellért <devgellert@gmail.com>
2022-01-09 06:14:21 +0100

    refact

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,37 +1,39 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {GetPizzaResponse, GetPizzasResponse} from "../types/api";
+import { HttpClient } from '@angular/common/http';
+import { GetPizzaResponse, GetPizzasResponse } from '../types/api';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
   static BASE_URL = 'http://localhost:5000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   public async getPizzas(): Promise<GetPizzasResponse | null> {
     const route = '/api/pizza/';
     const observable = this.http.get(ApiService.BASE_URL + route);
 
-    return new Promise((resolve, reject) => {
-      observable.subscribe({
-        next: (value) => {
-          resolve(value as GetPizzasResponse);
-        }
-      });
-    });
+    return this.withPromiseWrap<GetPizzasResponse>(observable);
   }
 
   getPizza(pizzaId: number): Promise<GetPizzaResponse> {
     const route = `/api/pizza/${pizzaId}`;
+
     const observable = this.http.get(ApiService.BASE_URL + route);
 
+    return this.withPromiseWrap(observable);
+  }
+
+  private withPromiseWrap<ResponseType extends any>(
+    observable: Observable<any>
+  ): Promise<ResponseType> {
     return new Promise((resolve) => {
       observable.subscribe({
         next: (value) => {
-          resolve(value as GetPizzaResponse);
-        }
+          resolve(value as ResponseType);
+        },
       });
     });
   }
